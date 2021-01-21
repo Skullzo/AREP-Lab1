c70a105a7f57a31be529814af8a592a71c4f6b03
Alejandro Toro Daza <skullzo@outlook.com>
2021-01-20 21:58:52 -0500

    Código Fuente Terminado

--- src/main/java/edu/escuelaing/arep/app/listas/ListaEnlazada.java
+++ src/main/java/edu/escuelaing/arep/app/listas/ListaEnlazada.java
@@ -1,13 +1,16 @@
 package main.java.edu.escuelaing.arep.app.listas;
 import java.util.Iterator;
-
+/**
+ * Clase encargada de llevar a cabo la Lista Enlazada, para luego proceder a realizar los calculos de la Media, Varianza y Desviación Estándar respectivamente.
+ * @author  Alejandro Toro Daza
+ * @version 1.0.  (20 de Enero del 2021) 
+ */
 public class ListaEnlazada<E> implements Iterable<E> {
     private Nodo<E> head = new Nodo<E>();
     private int size = 0;
-
     /**
-     * Agrega un nuevo nodo a la LinkedList.
-     * @param node El nuevo nodo a agregar
+     * Método encargado de agregar un nuevo nodo a la Lista Enlazada.
+     * @param node Parámetro que representa el nuevo nodo.
      * @return boolean
      */
     public boolean add(E node) {
@@ -22,11 +25,10 @@ public class ListaEnlazada<E> implements Iterable<E> {
         size++;
         return true;
     }
-
     /**
-     * Obtiene el elemento dentro de la LinkedList.
-     * @param index El indice que indica la posiciÃ³n del elemento que se quiere obtener.
-     * @return Dato del nodo
+     * Método encargado de obtener un elemento dentro de la Lista Enlazada.
+     * @param index Parámetro encargado de indicar la posición del elemento en cuestión.
+     * @return Retorna el dato del nodo respectivamente.
      */
     public E get(int index) {
         if (index < 0 || index < size) {
@@ -41,18 +43,16 @@ public class ListaEnlazada<E> implements Iterable<E> {
             throw new IndexOutOfBoundsException();
         }
     }
-
     /**
-     * Obtiene el tamaÃ±o que tiene la linkedList.
-     * @return El tamaÃ±o que tiene la linkedList.
+     * Método encargado de obtener el tamaño que tiene la Lista Enlazada.
+     * @return Retorna el tamaño que tiene la Lista Enlazada.
      */
     public int getSize() {
         return size;
     }
-
     /**
-     * Crea un iterador para desplazarce entre los elementes de la LinkedList.
-     * @return LinkedListIterator
+     * Método encargado de crear un iterador para desplazarse entre los elementos de la Lista Enlazada.
+     * @return Retorna el Iterador de la Lista Enlazada.
      */
     public Iterator<E> iterator()
     {
